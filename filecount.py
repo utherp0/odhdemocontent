89f505028de2dcce305aa476313848ff97d2ec4a
utherp0 <ian.lawson@redhat.com>
2023-06-12 10:18:17 +0100

    Added file example

---
+++ filecount.py
@@ -0,0 +1,10 @@
+import os
+
+dir_path = r'./singledata/'
+count = 0
+# Iterate directory
+for path in os.listdir(dir_path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dir_path, path)):
+        count += 1
+print('File count:', count)
